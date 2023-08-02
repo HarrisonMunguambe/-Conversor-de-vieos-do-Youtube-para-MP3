54f87075c839a29a4247a34fac8d271ab113959f
Harrison Munguambe <103441959+HarrisonMunguambe@users.noreply.github.com>
2023-08-02 15:24:20 +0200

    Documentacao melhorada do code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// importar bibliotecas que vamos usar
 import React, { useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { ScaleLoader } from "react-spinners";
@@ -5,20 +6,29 @@ import "./index.css";
 import { youtube_parser } from "./utils";
 import axios from "axios";
 
+//componete App.
 function App() {
-  const inputURLRef = useRef();
+  const inputURLRef = useRef(); // criar e declarar a variavel que vai controlar o que e introduzido no input do formulario
+  // Referência para o input do formulário
+
   const [urlResult, setUrlResult] = useState(null);
-  const [loading, setLoading] = useState(false); // Estado para controlar o efeito de loading
+  // Estado para armazenar a URL do vídeo
+  //criar um array com 2 elementos que urlResult vai ser o elemento que vai armazenar a url e o setUrlResult vai colocar o estado como ativo ou nao, por isso iniciamos com null.
+  const [loading, setLoading] = useState(false); // Estado para controlar o efeito de loading, loading e o elemento para armazenar o efeito e steLoading vai estar a controlar se esta ativo ou nao.
   const [videoNotFound, setVideoNotFound] = useState(false); // Estado para controlar a mensagem de vídeo não encontrado
 
+  // Função para lidar com o envio do formulário
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true); // Ativa o efeito de loading enquanto aguarda a resposta da API
+    e.preventDefault(); // evitar o comportamento padrão de atualizar a página quando o formulário é enviado
+
+    setLoading(true); // Ativa o efeito de loading enquanto aguarda a resposta da API, logo que recebe ela passa a true que e trocada abixo no code
+
     setVideoNotFound(false); // Reseta o estado videoNotFound
 
-    const youtubeID = youtube_parser(inputURLRef.current.value);
+    const youtubeID = youtube_parser(inputURLRef.current.value); // colocar na constante youtubeID o input recebido e cortado pelo metodo youtube_parser.
 
     const options = {
+      // Configurações para fazer a chamada à API de download do YouTube
       method: "get",
       url: "https://youtube-mp36.p.rapidapi.com/dl",
       headers: {
@@ -29,12 +39,14 @@ function App() {
         id: youtubeID,
       },
     };
+    // Faz a chamada à API e trata a resposta
     axios(options)
       .then((res) => {
         setLoading(false); // Desativa o efeito de loading quando a resposta da API é obtida
         if (res.data.error) {
           setVideoNotFound(true); // Define o estado videoNotFound como true se o vídeo não for encontrado
         } else {
+          // Define a URL do vídeo para possibilitar o download
           setUrlResult(res.data.link);
         }
       })
@@ -43,16 +55,16 @@ function App() {
         setLoading(false); // Certifique-se de desativar o efeito de loading em caso de erro
       });
 
-    inputURLRef.current.value = "";
+    inputURLRef.current.value = ""; // colocar a input do campo de input limpa logo depois de clicar pesquisar
   };
 
   const handleDownloadClick = () => {
     if (urlResult) {
       // Oculta o botão de download após o clique
-      setUrlResult(null);
+      setUrlResult(null); // vai passar setUrlResult para null e assim o botao de dowload nao sera exibido
     }
   };
-
+  // o que o componente vai retornar
   return (
     <div className="app">
       <span className="about">
@@ -73,8 +85,10 @@ function App() {
         </p>
 
         <form onSubmit={handleSubmit} className="form">
+          {/* ao subemeter o formulario vai chamara funcao handleSubmit */}
+
           <input
-            ref={inputURLRef}
+            ref={inputURLRef} // vai colocar como link/ valor do input o link que foi cortado pela metodo exterior
             placeholder="Copie e cole aqui o link do vídeo"
             className="form_input"
             type="text"
@@ -83,16 +97,18 @@ function App() {
             Pesquisar
           </button>
           {/* Renderiza a mensagem de vídeo não encontrado se o estado "videoNotFound" for true */}
-          {videoNotFound && !loading && (
-            <div className="download_btn">Vídeo indisponível</div>
-          )}
+          {videoNotFound &&
+            !loading && ( // se videoNotFound for true , e o efeito loading for false ele vai renderizar a div
+              <div className="download_btn">Vídeo indisponível</div>
+            )}
           {/* Renderiza o efeito de loading se o estado "loading" for true */}
           {loading && (
             <div className="loading">
               <ScaleLoader size={10} color={"#333"} loading={true} />
+              {/* {componete da biblioteca react spinner para renderizar um efeito bonito de loading} */}
             </div>
           )}
-          {/* Renderiza o link de download apenas se "urlResult" for verdadeiro */}
+          {/* Renderiza o link de download apenas se "urlResult" for verdadeiro se e loading e videoNotFound estiverem falsos e a nao serem renderizados*/}
           {urlResult && !loading && !videoNotFound && (
             <a
               href={urlResult}
@@ -100,6 +116,7 @@ function App() {
               rel="noreferrer"
               className="download_btn a"
               onClick={handleDownloadClick}
+              // verificar que o botao download vai ser removido depois de um download
             >
               Baixar o MP3
             </a>
